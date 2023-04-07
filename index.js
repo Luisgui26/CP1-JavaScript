1ce18823844b0d93276147334db7175751f3b93c
luis <luisguibarreto8@gmail.com>
2023-04-07 11:39:59 -0300

    Terceira Questão

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-/* 
-Questão 1
+
+/*
 let minhaVariavel;
-document.write(minhaVariavel);
+console.log(minhaVariavel);
 */
 
+/*
 let a = 13;
 let b = 5;
 let resultado = a % b;
 document.write(resultado);
+*/
 
+let peso = parseFloat(prompt("Digite o seu peso (em kg):"));
+let altura = parseFloat(prompt("Digite a sua altura (em metros):"));
 
+let imc = peso / (altura ** 2);
 
-
-
-
-
+if (imc < 18.5) {
+  alert("Seu IMC é " + imc.toFixed(2) + ", você está abaixo do peso ideal.");
+} else if (imc >= 18.5 && imc <= 24.9) {
+  alert("Seu IMC é " + imc.toFixed(2) + ", você está no peso ideal.");
+} else {
+  alert("Seu IMC é " + imc.toFixed(2) + ", você está acima do peso ideal.");
+}
